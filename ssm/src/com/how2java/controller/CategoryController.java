518285b37eff04d6b86705e60ab44feb7238f582
dongyatao <13717936127@163.com>
2019-02-03 17:11:23 +0800

    6666

--- ssm/src/com/how2java/controller/CategoryController.java
+++ ssm/src/com/how2java/controller/CategoryController.java
@@ -15,7 +15,7 @@ import com.how2java.service.CategoryService;
 public class CategoryController {
 	@Autowired
 	CategoryService categoryService;
-//aaaa
+	//aaaab
 	@RequestMapping("listCategory")
 	public ModelAndView listCategory(){
 		ModelAndView mav = new ModelAndView();
